5aaad3baff3e0e6431b3ceb2514f3d3f89795abd
c0d3m0nky <qbanbpc@gmail.com>
2023-06-10 01:20:25 -0400

    dockerfile

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.11
+
+WORKDIR /abec.io
+
+COPY ./src /abec.io/src
+COPY ./src/manage.py /abec.io/manage.py
+COPY ./src/requirements.txt /abec.io/requirements.txt
+
+RUN pip install --no-cache-dir --upgrade -r /abec.io/requirements.txt
+
+CMD pip install --no-cache-dir --upgrade -r /abec.io/requirements.txt && python manage.py runserver 80
--- src/settings.py
+++ src/settings.py
@@ -11,9 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+if os.environ.get('DB_DIR'):
+    DB_DIR = Path(os.environ.get('DB_DIR')).expanduser().resolve()
+else:
+    DB_DIR = BASE_DIR
 
 
 # Quick-start development settings - unsuitable for production
@@ -76,7 +82,7 @@ WSGI_APPLICATION = 'src.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': DB_DIR / 'db.sqlite3',
     }
 }
 
