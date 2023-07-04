9abd91f753d43c3c0bb1365e095ed88604622ecb
c0d3m0nky <qbanbpc@gmail.com>
2023-07-04 01:30:02 -0400

    Reworked site with new bootstrap theme

--- static/views.py
+++ static/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, "static/index.html", {})
 
+
+def resume(request):
+    return render(request, "static/resume.html", {})
+
+# D&D combat time tracker
+
